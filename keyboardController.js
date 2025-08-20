beeb0497319aea332351c853bc3808d7330906cf
Yang L <s3354967@student.rmit.edu.au>
2025-08-20 15:59:50 +1000

    update js
    update js

--- keyboardController.js
+++ keyboardController.js
@@ -10,10 +10,56 @@ let allKeys = Array.from(document.getElementsByClassName("whiteKey")).concat(
 /* set default octace : we will update based on keys later on */
 let octave = 3;
 
+let mouseHeld = false;
+
+window.addEventListener("mousedown", () => {
+  mouseHeld = true;
+});
+
+window.addEventListener("mouseup", () => {
+  mouseHeld = false;
+});
 /* add an event listener to each key */
 allKeys.forEach((key) => {
   key.addEventListener("mousedown", (e) => {
     let note = e.target.dataset.note;
-    polySynth.triggerAttackRelease(note + octave, "8n");
+    polySynth.triggerAttack(note + octave);
   });
+  key.addEventListener("mouseup", (e) => {
+    let note = e.target.dataset.note;
+    polySynth.triggerRelease(note + octave);
+  });
+  key.addEventListener("mouseevent", (e) => {
+    if (mouseHeld === false) {
+      return;
+    }
+    let note = e.target.dataset.note;
+    polySynth.triggerAttack(note + octave);
+  });
+  key.addEventListener("mouseleave", (e) => {
+    let note = e.target.dataset.note;
+    polySynth.triggerRelease(note + octave);
+  });
+});
+
+let keyHeld = false;
+
+// event listener for keyboard(qwerty)//
+
+window.addEventListener("keydown", (e) => {
+  console.log(e);
+  if (keyCodeToNote[e.code]) {
+    if (keyHeld === false) {
+      polySynth.triggerAttack(keyCodeToNote[e.code]);
+      keyHeld = true;
+    }
+  }
+});
+
+window.addEventListener("keyup", (e) => {
+  console.log(e);
+  if (keyCodeToNote[e.code]) {
+    polySynth.triggerRelease(keyCodeToNote[e.code]);
+    keyHeld = false;
+  }
 });
